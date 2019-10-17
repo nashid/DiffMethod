5d9ecb809a43f1cdec01b1cb0403fdb1c38110dc
Ehsan <Ehsan@MacBook-Pro.local>
2019-10-18 01:13:33 +0330

    Add results, fix the readme

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # DiffMethod
 Finding differences between arguments of methods
+
+
+## How to build?
+
+1. **Clone the repository**
+
+ Add it in your project level build.gradle:
+```git
+    git clone https://github.com/EhsanMashhadi/DiffMethod.git
+```
+
+2. **Navigate to the project root directory**
+
+
+3. **Build a jar file**
+
+```
+./gradlew fatjar
+```
+
+## How to use?
+
+1. **Navigate to the lib folder**
+
+```
+/project folder/build/libs
+```
+
+2. **Run a jar file**
+
+```
+java -jar CodingTask-all-1.0.0.jar
+```
+
+## Want an easier way?
+
+Download the jar file from the release tab and use it
+
+## Result
+
+I have tested this tool with 4 repositories:
+
+1. **My test repo**
+
+* URL: <https://github.com/EhsanMashhadi/test>
+    
+* Result: <https://github.com/EhsanMashhadi/DiffMethod/result/test.csv>
+    
+2. **Gson**
+
+* URL: <https://github.com/google/gson>
+    
+* Result: <https://github.com/EhsanMashhadi/DiffMethod/result/gson.csv>
+
+3. **Retrofit**
+
+* URL: <https://github.com/square/retrofit>
+    
+* Result: <https://github.com/EhsanMashhadi/DiffMethod/result/retrofit.csv>
+    
+4. **Streamex**
+
+* URL: <https://github.com/amaembo/streamex>
+    
+* Result: <https://github.com/EhsanMashhadi/DiffMethod/result/streamex.csv>
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -7,10 +7,11 @@ public class MainClass {
         String url;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the specific url of project or choose from these options \n" +
+        System.out.println("Enter the specific url or choose from these options: \n\n" +
                 "1) My test (https://github.com/amaembo/streamex.git) \n" +
                 "2) StreamX (https://github.com/EhsanMashhadi/test.git) \n" +
-                "3) Gson (https://github.com/google/gson)");
+                "3) Gson (https://github.com/google/gson)) \n" +
+                "4) Retrofit (https://github.com/square/retrofit)");
 
         String inputUrl = scanner.next();
 
@@ -24,27 +25,36 @@ public class MainClass {
                 url = "https://github.com/amaembo/streamex.git";
                 break;
             }
+            case "3": {
+                url = "https://github.com/google/gson)";
+                break;
+            }
+            case "4": {
+                url = "https://github.com/square/retrofit";
+                break;
+            }
             default: {
                 url = inputUrl;
             }
         }
 
-        System.out.println("Enter mName of clone folder? Press D for default");
+        System.out.println("Enter name of clone folder? Press D for default");
         String cloneFolder = scanner.next();
 
         if (cloneFolder.equals("D")) {
-            cloneFolder = "test-clone";
+            cloneFolder = "clone";
         }
 
-        System.out.println("Enter mName of the output file? Press enter D for default");
+        System.out.println("Enter name of the output file? Press D for default");
         String outputFile = scanner.next();
 
         if (outputFile.equals("D")) {
-            outputFile = "test-output";
+            outputFile = "result";
         }
 
         MainClass mainClass = new MainClass();
         mainClass.fetchRemote(url, cloneFolder, outputFile + ".csv");
+
     }
 
     private void fetchRemote(String url, String cloneFolder, String outputFile) {
